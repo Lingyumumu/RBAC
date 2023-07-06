55632eef3f5b7b85a12f2df30b7331f5c7662581
Lingyumumu <102763725+Lingyumumu@users.noreply.github.com>
2023-07-06 22:25:53 +0200

    1234

--- PHP/Administrateur/user/deletePage.php
+++ PHP/Administrateur/user/deletePage.php
@@ -1,13 +1,16 @@
 <?php 
 include '../../dbConn.php';
 session_start();
+$id = $_GET['ID'];
 if ($_SESSION['role'] != 'administrateur') {
     header("location: ../../login.php");
 }
-$id = $_GET['ID'];
-if (isset($_GET['ID']) == null ){
-    header("location: list_user.php");
-}
+$querynom = "SELECT * FROM user WHERE ID = '$id'";
+$resultnom = mysqli_query($connection, $querynom);
+$rownom = mysqli_fetch_assoc($resultnom);
+
+$queryprof = "DELETE FROM cours WHERE nom_prof = '$rownom[nom]'";
+$resultprof = mysqli_query($connection, $queryprof);
 $query = "DELETE FROM user WHERE ID = $id";
 if(mysqli_query($connection, $query)){
     header("location: list_user.php");
